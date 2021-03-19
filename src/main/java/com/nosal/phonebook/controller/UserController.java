af3ebe9ecb860e853e39719e91963e50c3393f74
Nosal, Bohdan [PS] <bohdan.nosal@cengage.com>
2021-03-19 15:57:32 +0200

    Small change for jenkins

--- src/main/java/com/nosal/phonebook/controller/UserController.java
+++ src/main/java/com/nosal/phonebook/controller/UserController.java
@@ -3,6 +3,7 @@ package com.nosal.phonebook.controller;
 import com.nosal.phonebook.model.User;
 import com.nosal.phonebook.service.UserService;
 import java.util.List;
+import javax.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
@@ -24,7 +25,7 @@ public class UserController {
     private UserService userService;
 
     @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<?> add(@RequestBody User user) {
+    public ResponseEntity<?> add(@RequestBody @NotNull User user) {
         log.info("UserController#add user {}", user);
         userService.save(user);
         return new ResponseEntity<>(HttpStatus.OK);
